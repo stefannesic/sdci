96e5c114a30fb284f60e31e8129054aa91b0e4c1
hnerdrum <henrik.nerdrum@gmail.com>
2020-01-18 11:36:19 +0100

    getLatency now returns rtt

--- monitoring.js
+++ monitoring.js
@@ -3,22 +3,21 @@ const request = require('request-promise');
 const args = process.argv.splice(2);
 
 async function requestHelper(url) {
-	return request({ url: url })
+	return request({ url: url, time: true })
 		.then(response => {
 			return JSON.parse(response)["pong"];
 		})
 		.catch(err => { console.log(err) });
 
+
 }
 
 async function getLatency(url, win) {
-	let count = 0;
 	let res = 0;
 	for(i = 0; i < win; i++) {
 		before = Date.now();
 		after = await requestHelper(url);
-		res += after - before;
-		console.log(res)
+		res += Date.now() - before;
 	}
 
 	return res / win;
