4c2743ea96bc13a0f6b043a64a1cd9ce2a79c0bc
hnerdrum <henrik.nerdrum@gmail.com>
2020-01-18 18:49:19 +0100

    Fix Dockerfile once again

---
+++ gc.js
@@ -0,0 +1,28 @@
+const request = require('request-promise');
+
+const args = process.argv.splice(2);
+
+async function requestHelper(url) {
+	return request({ url: url, time: true })
+		.then(response => {
+			return JSON.parse(response)["pong"];
+		})
+		.catch(err => { console.log(err) });
+
+
+}
+
+async function getLatency(url, win) {
+	let res = 0;
+	for(i = 0; i < win; i++) {
+		before = Date.now();
+		after = await requestHelper(url);
+		res += Date.now() - before;
+	}
+
+	return res / win;
+}
+
+async function insert_load_balancer()
+
+getLatency(args[0], args[1]).then(res => console.log(res));
