f026e49a6abdcad3f7b8edf7829ce638e53a6be8
hnerdrum <henrik.nerdrum@gmail.com>
2020-01-18 11:22:17 +0100

    Fix getLatency

--- monitoring.js
+++ monitoring.js
@@ -1,27 +1,27 @@
-const request = require('request');
+const request = require('request-promise');
 
 const args = process.argv.splice(2);
 
+async function requestHelper(url) {
+	return request({ url: url })
+		.then(response => {
+			return JSON.parse(response)["pong"];
+		})
+		.catch(err => { console.log(err) });
+
+}
+
 async function getLatency(url, win) {
+	let count = 0;
 	let res = 0;
 	for(i = 0; i < win; i++) {
-		const before = Date.now();
-		request(url, time = true, function(err, response) {
-			after = JSON.parse(response.body)["pong"];
-			res += after - before
-			console.log(i)
-			if(i === (win-1)) {
-				console.log(res);
-				return res / win;
-			}
-		})
+		before = Date.now();
+		after = await requestHelper(url);
+		res += after - before;
+		console.log(res)
 	}
+
 	return res / win;
 }
 
-async function main() {
-	let res = await getLatency(args[0], args[1]);
-	console.log(res);
-}
-
-main()
+getLatency(args[0], args[1]).then(res => console.log(res));
